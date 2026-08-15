798db15502600f94c7504afae20359b360a84dc2
TJBLUE99 <tejaswanaik.1999@gmail.com>
2026-08-15 22:31:02 +0530

    added context task decorator

--- invoice/src/main/java/com/vulnerable/invoice/TenantContext/ContextPropagatingTaskDecorator.java
+++ invoice/src/main/java/com/vulnerable/invoice/TenantContext/ContextPropagatingTaskDecorator.java
@@ -1,4 +1,43 @@
 package com.vulnerable.invoice.TenantContext;
 
-public class ContextPropagatingTaskDecorator {
+import org.jspecify.annotations.NonNull;
+import org.slf4j.MDC;
+import org.springframework.core.task.TaskDecorator;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Map;
+
+public class ContextPropagatingTaskDecorator implements TaskDecorator {
+    @Override
+    @NonNull
+    public Runnable decorate(@NonNull Runnable runnable) {
+        // 1. Capture context from the caller (HTTP) thread
+        String callerTenantId = TenantContext.getTenantId();
+        SecurityContext callerSecurityContext = SecurityContextHolder.getContext();
+        Map<String, String> callerMdcContext = MDC.getCopyOfContextMap();
+
+        return () -> {
+            try {
+
+                if (callerTenantId != null) {
+                    TenantContext.setTenantId(callerTenantId);
+                }
+                if (callerSecurityContext != null && callerSecurityContext.getAuthentication() != null) {
+                    SecurityContextHolder.setContext(callerSecurityContext);
+                }
+                if (callerMdcContext != null) {
+                    MDC.setContextMap(callerMdcContext);
+                }
+
+                runnable.run();
+
+            } finally {
+                TenantContext.clearTenantId();
+                SecurityContextHolder.clearContext();
+                MDC.clear();
+            }
+        };
+    }
+
 }
